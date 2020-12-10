446fa1be87bbb99d56e825742fe6901e461d40bb
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-12-10 17:59:16 +0530

    Chngs

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { Route } from "react-router-dom";
+import "../styles/App.css";
+import Home from "./home";
+import About from "./about";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" componenet={About} />
+      </div>
+    );
+  }
 }
-
 
 export default App;
